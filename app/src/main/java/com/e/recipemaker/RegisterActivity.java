0ece484a0e7eb45e39c4903ef97ad87dc9974805
anheyman <antoineheymans@yahoo.fr>
2019-11-09 00:38:38 +0100

    add registration csv file+ write/read method

--- app/src/main/java/com/e/recipemaker/RegisterActivity.java
+++ app/src/main/java/com/e/recipemaker/RegisterActivity.java
@@ -6,7 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -20,9 +28,59 @@ public class RegisterActivity extends AppCompatActivity {
         System.out.println("ON EST AL");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register1);
+        editText = (EditText)findViewById(R.id.editText);
+        editText2 = (EditText)findViewById(R.id.editText2);
 
 
     }
+
+    public void next_clicked(View view) {
+        String fileName = "dbUsername.csv";
+        final String filePath = this.getFilesDir().getPath().toString() + fileName;
+
+
+        File file = new File(filePath);
+        boolean exists = file.exists();
+        if (!exists) {
+            writeFile(file, new String[]{"admin", "a"});
+        }
+        String[] record = {editText.getText().toString(),editText2.getText().toString()};
+        if(!record[0].isEmpty() && !record[1].isEmpty() ) {
+            writeFile(file, record);
+            allergiesform(view);
+        }
+
+        readFile(file);
+        System.out.println(filePath);
+    }
+
+    public void writeFile(File file, String[] record){
+        try (CSVWriter writer = new CSVWriter(new FileWriter(file, true))){
+            writer.writeNext(record);
+            System.out.println(record);
+        } catch (IOException e) {
+            System.out.println("echec d'écriture");
+            e.printStackTrace();
+        }
+    }
+
+    public void readFile(File file){
+        try (CSVReader reader = new CSVReader(new FileReader(file))) {
+            System.out.println("lecture...");
+            //Read CSV line by line and use the string array as you want
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                if (nextLine != null) {
+                    //Verifying the read data here
+                    System.out.println(Arrays.toString(nextLine));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("echec de lecture");
+            e.printStackTrace();
+        }
+    }
+
 
     public void allergiesform(View view) {
         Intent intent = new Intent(this, AllergiesActivity.class);
