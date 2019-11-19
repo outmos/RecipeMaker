390229c3ce96350c01526a52066cf000a0fd3312
anheyman <antoineheymans@yahoo.fr>
2019-11-19 15:10:23 +0100

    correct adding recipe to favorite

--- app/src/main/java/com/e/recipemaker/RecipeActivity.java
+++ app/src/main/java/com/e/recipemaker/RecipeActivity.java
@@ -11,6 +11,8 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -51,7 +53,8 @@ public class RecipeActivity extends AppCompatActivity {
         addToFavoriteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DatabaseReference recipesReference = FirebaseDatabase.getInstance().getReference().child("Users").child("recipes");
+                final String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                DatabaseReference recipesReference = FirebaseDatabase.getInstance().getReference().child("Users").child(userID).child("recipes");
                 recipesReference = recipesReference.child(recipe.getId());
                 recipesReference.setValue(recipe.getName());
                 Toast.makeText(RecipeActivity.this,"The recipe has been added to your favourites !",Toast.LENGTH_LONG).show();
